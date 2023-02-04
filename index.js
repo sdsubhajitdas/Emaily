9b5993868c46419a05a068b5e1ab335817a15e92
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-02-04 20:01:20 +0530

    Files refactored

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT;
 app.listen(PORT, (err) => {
   if (err) console.error("Error in server setup. Server not running.");
