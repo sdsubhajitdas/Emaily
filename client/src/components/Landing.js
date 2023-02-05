2d2c9950a9060b5049624da7f0df9e8999efc611
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-02-05 23:15:30 +0530

    Fixed landing page height

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,12 @@
-// import { useSelector } from "react-redux";
-
 export default function Landing() {
-  // const auth = useSelector((state) => state.auth.value);
-
   return (
-    <div className="text-center p-5">
-      <h1>Welcome to Emaily!!</h1>
-      Collect feedback from your users
+    <div className="hero">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="font-bold">Welcome to Emaily!!</h1>
+          <p className="py-4">Collect feedback from your users</p>
+        </div>
+      </div>
     </div>
   );
 }
